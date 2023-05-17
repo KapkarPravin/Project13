de270e77312ae9068fa42f6320290b16a30fd48c
KapkarPravin <pravin.kapkar26@gmail.com>
2023-05-17 23:58:21 +0530

    This is my class3

---
+++ Class3.java
@@ -0,0 +1,43 @@
+package package3;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//data base in java programming
+        // we are learning database in java programming 
+		//we are studying the boolean data types 
+		boolean israining = true ; // declearation and initialization 
+		System.out.println(israining);
+		israining = false ;
+		System.out.println(israining);
+		
+		// byte data type
+		byte marks = 100 ;
+		System.out.println(marks);
+		
+		//short data type
+		short villagepop =15325;
+		System.out.println(villagepop);
+		
+		//int data type 
+		int nooffollowers = 102354035;
+		System.out.println(nooffollowers);
+		
+		// long data type 
+		long pop = 720105360253L;
+		System.out.println(pop);
+		
+		// float data type
+		float temp = 35.25f;
+		System.out.println(temp);
+		
+		// double data type
+		double humidity = 6.358;
+		System.out.println(humidity);
+		
+
+	}
+
+}
